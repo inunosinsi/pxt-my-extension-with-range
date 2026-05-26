b38deef14d571b36ce7f971e6cb22d4ede672d73
Tsuyoshi Saito <tsuyoshi@saitodev.co>
2026-05-26 12:09:04 +0900

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
-basic.forever(function () {
-	
-})
+//% color="#000000" icon="\uf1e6" block="値の範囲付き拡張機能"
+namespace myFirstExtension {
+
+    //% block="range %n"
+    //% n.min=0 n.max=10
+    export function range(n: number): void {
+        basic.showNumber(n)
+    }
+}
